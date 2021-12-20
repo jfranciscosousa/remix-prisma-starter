f5df540488120213be79dfb07b0876967ab38914
Francisco Sousa <francisco.sousa@hey.com>
2021-12-20 19:11:11 +0000

    Setup automatic dotenv

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
