2c2d92ffc8e391fdbc4205b88b7ce1a7ec29d83b
Francisco Sousa <francisco@remote.com>
2022-02-20 13:03:34 +0000

    Update remix versions

--- api/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const { createRequestHandler } = require("@remix-run/vercel");
-
-module.exports = createRequestHandler({
-  build: require("./build"),
-});
