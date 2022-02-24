5750dd0d1f87206e5672e7beb95fc69ac38a37ef
Francisco Sousa <francisco.sousa@hey.com>
2022-02-24 22:29:48 +0000

    Argon2

--- app/data/passwordUtils.server.ts
+++ app/data/passwordUtils.server.ts
@@ -1,22 +1,12 @@
-import crypto from "crypto";
-import { promisify } from "util";
-
-const scrypt = promisify(crypto.scrypt);
+import argon from "argon2";
 
 export async function encryptPassword(password: string): Promise<string> {
-  const salt = crypto.randomBytes(8).toString("hex");
-  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
-
-  return `${salt}:${derivedKey.toString("hex")}`;
+  return argon.hash(password);
 }
 
 export async function verifyPassword(
   hash: string,
   password: string
 ): Promise<boolean> {
-  const [salt, key] = hash.split(":");
-  const keyBuffer = Buffer.from(key, "hex");
-  const derivedKey = (await scrypt(password, salt, 64)) as Buffer;
-
-  return crypto.timingSafeEqual(keyBuffer, derivedKey);
+  return argon.verify(hash, password);
 }
