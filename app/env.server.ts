30490f65c2bf4a6ed480d46847e656405e8aff75
Francisco Sousa <francisco.sousa@hey.com>
2023-02-21 00:11:31 +0000

    Boolean handling

--- app/env.server.ts
+++ app/env.server.ts
@@ -4,10 +4,7 @@ import { generateErrorMessage } from "zod-error";
 export const serverEnvSchema = z.object({
   DATABASE_URL: z.string(),
   MIGRATE_DATABASE_URL: z.string().optional(),
-  SECURE_AUTH_COOKIE: z.preprocess(
-    (v) => (v === "true" ? true : false),
-    z.boolean()
-  ),
+  SECURE_AUTH_COOKIE: z.enum(["true", "false"]).transform(Boolean),
   NODE_ENV: z.enum(["development", "test", "production"]),
   SECRET_KEY_BASE: z.string(),
 });
