ef34f39f2f65b8196d4828f64e00b076c15beb96
Francisco Sousa <francisco.sousa@hey.com>
2023-05-03 18:12:54 +0100

    Update remix

--- remix.config.js
+++ remix.config.js
@@ -2,19 +2,14 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildPath: process.env.VERCEL_ENV ? "api/index.js" : "build/index.js",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.VERCEL_ENV ? "./server.js" : undefined,
   ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  publicPath: "/build/",
+  server: process.env.VERCEL_ENV ? "./server.js" : undefined,
+  serverBuildPath: process.env.VERCEL_ENV ? "api/index.js" : "build/index.js",
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+  tailwind: true,
   future: {
-    unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
     v2_meta: true,
